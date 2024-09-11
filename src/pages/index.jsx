0c3eafd4e27fd5b6c570d366bb4b53270db4935b
AbdulHaseebKashmirii <k225069@nu.edu.pk>
2024-09-12 01:36:04 +0500

    8th

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -118,10 +118,10 @@ function Newsletter() {
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
+        <span className="ml-3">Subscribe to my newsletter!</span>
       </h2>
       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
+        Get notified when I publish something new.
       </p>
       <div className="mt-6 flex">
         <input
