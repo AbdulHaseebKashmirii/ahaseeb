63d24b6ce1496c8342b452ac25420da4ca80a582
AbdulHaseebKashmirii <k225069@nu.edu.pk>
2024-10-06 00:11:15 +0500

    automating commits with run.bat

--- src/pages/next-seo.config.js
+++ src/pages/next-seo.config.js
@@ -2,7 +2,7 @@ export default {
     openGraph: {
       type: 'website',
       locale: 'en_IE',
-      url: 'https://www.ahaseeb.com',
+      url: 'https://www.ahaseeb.com/',
       siteName: 'Abdul Haseeb',
     },
     twitter: {
