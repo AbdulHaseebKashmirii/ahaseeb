6f1fd79a04f5c52f7353ae184385480c34a7eb29
AbdulHaseebKashmirii <k225069@nu.edu.pk>
2024-10-06 00:30:37 +0500

    automating commits with run.bat

--- src/pages/thank-you.jsx
+++ src/pages/thank-you.jsx
@@ -14,7 +14,7 @@ export default function ThankYou() {
       </Head>
       <SimpleLayout
         title="Thanks for hopping on board! 🎉"
-        intro="I can’t wait to share my adventures as a data scientist, developer, and founder with you. You’ll get the inside scoop whenever I launch a new project, drop some cool insights, or have exciting news to share. And don’t worry, if you ever want to jump ship, you can unsubscribe anytime—no hard feelings!"
+        intro="I can’t wait to share my adventures as a data scientist, developer, and founder with you. You’ll get the inside scoop whenever I launch a new project, drop some cool insights, or have exciting news to share. And don’t worry, if you ever want to jump ship, you can unsubscribe anytime, no hard feelings!"
       />
     </>
   )
