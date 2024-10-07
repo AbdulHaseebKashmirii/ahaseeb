765fa05e98682c67ced1057e9718f5135fa6adb4
AbdulHaseebKashmirii <k225069@nu.edu.pk>
2024-10-08 01:20:52 +0500

    automating commits with run.bat

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -79,24 +79,16 @@ export default function About() {
             </h1>
             <div className="mt-6 text-lg prose space-y-7 dark:prose-invert text-zinc-600 dark:text-zinc-400">
             <p>
-              I initially started as an engineering student, but my interest in technology led me to pursue a BSc in Computer Science at FAST NUCES. This marked a significant shift in my career path and was a deliberate choice to follow my passion. 
-              During my time at university, I focused extensively on mastering the fundamentals of programming languages such as C, C++, and Assembly Language. These languages helped me build a strong foundation in computer science, especially in areas like Data Structures, 
-              which are essential for problem-solving and algorithmic thinking.
+              I initially started as an engineering student, but my interest in technology led me to pursue a BSc in Computer Science at FAST NUCES. This marked a significant shift in my career path and was a deliberate choice to follow my passion. During my time at university, I focused extensively on mastering the fundamentals of programming languages such as C, C++, and Assembly Language. These languages helped me build a strong foundation in computer science, especially in areas like Data Structures, which are essential for problem-solving and algorithmic thinking.
             </p>
             <p>
-              Outside of coursework, I was highly involved in coding competitions. These fast-paced and challenging events reflected my competitive spirit and drive for continuous improvement. Additionally, 
-              I took the initiative to teach myself Python, a highly versatile and popular programming language used in data analysis and machine learning.
-              This self-motivation and adaptability to new tools and technologies have been essential in my learning journey.
+              Outside of coursework, I was highly involved in coding competitions. These fast-paced and challenging events reflected my competitive spirit and drive for continuous improvement. Additionally, I took the initiative to teach myself Python, a highly versatile and popular programming language used in data analysis and machine learning. This self-motivation and adaptability to new tools and technologies have been essential in my learning journey.
             </p>
             <p>
-              A major turning point for me was receiving a scholarship from DataCamp, a leading platform for data science education.
-               This opportunity allowed me to delve deeper into data analysis and equipped me with specialized skills in tools such as SQL, Tableau, and Power BI—essential for data visualization, database management, and making data-driven business decisions. I am passionate about the power of data, which I see not just as a technical tool but as a way to tell stories,
-              predict future trends, and address some of the world's most pressing issues, such as environmental protection, health innovation, and educational empowerment. 
+              A major turning point for me was receiving a scholarship from DataCamp, a leading platform for data science education. This opportunity allowed me to delve deeper into data analysis and equipped me with specialized skills in tools such as SQL, Tableau, and Power BI—essential for data visualization, database management, and making data-driven business decisions. I am passionate about the power of data, which I see not just as a technical tool but as a way to tell stories, predict future trends, and address some of the world's most pressing issues, such as environmental protection, health innovation, and educational empowerment. 
             </p>
             <p>
-             Overall, my journey reflects a blend of technical expertise, self-driven learning, and a strong sense of purpose. 
-             My focus on data analysis, paired with my passion for using technology to solve global challenges, 
-             sets me on a path toward making meaningful contributions in fields like environmental sustainability, healthcare, and education. 
+             Overall, my journey reflects a blend of technical expertise, self-driven learning, and a strong sense of purpose. My focus on data analysis, paired with my passion for using technology to solve global challenges, sets me on a path toward making meaningful contributions in fields like environmental sustainability, healthcare, and education.
             </p>
             </div>
           </div>
